0fc32ac156abdf1b7450ce50737cafc7e826861f
Lukas-JavaScript <lukaswittek96@gmail.com>
2025-10-07 18:01:45 +0200

    Added class in_progress_tasks and maked sth. in added_tasks

--- TaskMangement/manegement/models.py
+++ TaskMangement/manegement/models.py
@@ -1,10 +1,21 @@
+from datetime import date
+from datetime import datetime
 from django.db import models
 
 # Create your models here.
 class added_tasks(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
-    created_at_date = models.DateTimeField(auto_now_add=True)
+    created_at_date = models.DateTimeField(default=date.today)
+    created_at_time = models.TimeField(default=datetime.now)
 
     def __str__(self):
         return f"{self.name}"
+
+class in_progress_tasks(models.Model):
+    name = models.CharField(max_length=200)
+    created_at_date = models.DateTimeField()
+    created_at_time = models.TimeField()
+    description = models.TextField()
+    in_progress_at_date = models.DateTimeField(default=date.today)
+    in_progress_at_time = models.TimeField(default=datetime.now)
