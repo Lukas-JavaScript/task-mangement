a7860d90a3e708a79b8bcc9a81b3e9d26142c511
Lukas-JavaScript <lukaswittek96@gmail.com>
2025-10-07 18:38:06 +0200

    Added sth. for later admin interface

--- TaskMangement/manegement/models.py
+++ TaskMangement/manegement/models.py
@@ -14,18 +14,24 @@ class added_tasks(models.Model):
 
 class in_progress_tasks(models.Model):
     name = models.CharField(max_length=200)
+    description = models.TextField()
     created_at_date = models.DateTimeField()
     created_at_time = models.TimeField()
-    description = models.TextField()
     in_progress_at_date = models.DateTimeField(default=date.today)
     in_progress_at_time = models.TimeField(default=datetime.now)
 
+    def __str__(self):
+        return f"{self.name}"
+
 class completed_tasks(models.Model):
     name = models.CharField(max_length=200)
+    description = models.TextField()
     created_at_date = models.DateTimeField()
     created_at_time = models.TimeField()
-    description = models.TextField()
     in_progress_at_date = models.DateTimeField()
     in_progress_at_time = models.TimeField()
     completed_at_date = models.DateTimeField(default=date.today)
     completed_at_time = models.TimeField(default=datetime.now)
+
+    def __str__(self):
+        return f"{self.name}"
