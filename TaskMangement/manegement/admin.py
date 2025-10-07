fe03d3399b69e024b1b7ae97c0245efe908ae94c
Lukas-JavaScript <lukaswittek96@gmail.com>
2025-10-07 19:27:13 +0200

    fixed Bag

--- TaskMangement/manegement/admin.py
+++ TaskMangement/manegement/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import added_tasks, in_progress_tasks, completed_tasks
+from .models import added_tasks, in_progress_tasks, completed_tasks
 
 # Register your models here.
 admin.site.register(added_tasks)
