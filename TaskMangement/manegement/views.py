7e785533b5de207a1067512b95d5febc044e3d9f
Lukas-JavaScript <lukaswittek96@gmail.com>
2025-10-07 20:14:28 +0200

    linked class

--- TaskMangement/manegement/views.py
+++ TaskMangement/manegement/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .models import added_tasks, in_progress_tasks, completed_tasks, removed_tasks
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    added = added_tasks.objects.all()
+    in_progress = in_progress_tasks.objects.all()
+    completed = completed_tasks.objects.all()
+
+    return render(request, 'index.html', {
+        'added_tasks': added,
+        'in_progress_tasks': in_progress,
+        'completed_tasks': completed,
+    })
