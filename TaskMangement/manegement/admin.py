c5017b94aa62feb3ec5ac2af49408baa0e3c0b36
Lukas-JavaScript <lukaswittek96@gmail.com>
2025-10-07 19:48:36 +0200

    register removed_tasks

--- TaskMangement/manegement/admin.py
+++ TaskMangement/manegement/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import added_tasks, in_progress_tasks, completed_tasks
+from .models import added_tasks, in_progress_tasks, completed_tasks, removed_tasks
 
 # Register your models here.
 admin.site.register(added_tasks)
 admin.site.register(in_progress_tasks)
 admin.site.register(completed_tasks)
+admin.site.register(removed_tasks)
