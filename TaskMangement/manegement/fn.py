cf9a3479cb847ddfbbfb55e7d645e247431e9f0b
Lukas-JavaScript <lukaswittek96@gmail.com>
2025-10-10 18:57:22 +0200

    maked backend in_progress_tasks

--- TaskMangement/manegement/fn.py
+++ TaskMangement/manegement/fn.py
@@ -1,5 +1,15 @@
 from .models import added_tasks, in_progress_tasks, completed_tasks, removed_tasks
 
+
 def add_task(headline, description):
     print(f"Adding task: {headline} with description: {description}")
     added_tasks.objects.create(name=headline, description=description)
+
+
+def set_in_progress(task_id):
+    task = added_tasks.objects.get(id=task_id)
+    in_progress_tasks.objects.create(name=task.name,
+                                      description=task.description,
+                                      created_at_date=task.created_at_date,
+                                      created_at_time=task.created_at_time)
+    added_tasks.objects.get(id=task_id).delete()
--- TaskMangement/manegement/views.py
+++ TaskMangement/manegement/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import added_tasks, in_progress_tasks, completed_tasks
-from .fn import add_task
+from .fn import add_task, set_in_progress
 
 # Create your views here.
 def home(request):
@@ -9,6 +9,9 @@ def home(request):
             headline = request.POST.get('headline')
             description = request.POST.get('description')
             add_task(headline, description)
+        if request.POST.get('subject') == 'set_in_progress':
+            task_id = request.POST.get('id')
+            set_in_progress(task_id)
     added = added_tasks.objects.all()
     in_progress = in_progress_tasks.objects.all()
     completed = completed_tasks.objects.all()
