c44b367c8408e7952676945ea145b6d737bee11d
Lukas-JavaScript <lukaswittek96@gmail.com>
2025-10-10 17:15:54 +0200

    print when adding task

--- TaskMangement/manegement/fn.py
+++ TaskMangement/manegement/fn.py
@@ -1,4 +1,5 @@
 from .models import added_tasks, in_progress_tasks, completed_tasks, removed_tasks
 
-def add_task(name, description):
-    added_tasks.objects.create(name=name, description=description)
+def add_task(headline, description):
+    print(f"Adding task: {headline} with description: {description}")
+    added_tasks.objects.create(name=headline, description=description)
