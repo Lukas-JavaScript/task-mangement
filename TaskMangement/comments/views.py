30bfb3097b883a531ffa25f8ef45839a35f5e283
Lukas-JavaScript <lukaswittek96@gmail.com>
2025-10-12 15:14:33 +0200

    added Backend functionality

--- TaskMangement/comments/views.py
+++ TaskMangement/comments/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Comment
 
 # Create your views here.
 def comments(request):
-    return render(request, 'comments.html')
+    comments = Comment.objects.all()
+    if request.method == 'POST':
+        subject = request.POST.get('subject')
+        description = request.POST.get('description')
+        Comment.objects.create(subject=subject, description=description)
+    return render(request, 'comments.html', {'comments': comments})
