bc9128890dc569b7205bd887353e5bebb5af1213
Lukas-JavaScript <lukaswittek96@gmail.com>
2025-10-12 05:51:00 +0200

    added class Comment

--- TaskMangement/comments/models.py
+++ TaskMangement/comments/models.py
@@ -1,3 +1,10 @@
+from datetime import date, datetime
 from django.db import models
 
+def current_time():
+    return datetime.now().time()
 # Create your models here.
+class Comment(models.Model):
+    text = models.TextField()
+    created_at_date = models.DateField(default=date.today)
+    created_at_time = models.TimeField(default=current_time)
